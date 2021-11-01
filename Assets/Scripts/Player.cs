b2a4d00fc3327c04631126c67de4af5d222ea84c
PokeSpencerMx <91617455+PokeSpencerMx@users.noreply.github.com>
2021-11-01 09:26:18 -0400

    Progress for the inventory system

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private UI_Inventory uiInventory;
+    
+    private Inventory inventory;
+    private void Awake()
+    {
+        inventory = new Inventory();
+        uiInventory.SetInventory(inventory);
+    }
+}
